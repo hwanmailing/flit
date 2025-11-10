4df3802ac1d0b1b29f934a1471f49a7284804a29
hwanmailing <hwanmailing@github.com>
2025-11-10 23:27:05 +0900

    testing

--- info_.js
+++ info_.js
@@ -15,7 +15,7 @@ window.GAMES_DB_DATA = [
   {
     "idx": 2,
     "id": "RetroCircus",
-    "view_count": 1,
+    "view_count": 2,
     "like_count": 1,
     "score": 0,
     "rating_total": 0,
@@ -37,7 +37,7 @@ window.GAMES_DB_DATA = [
   {
     "idx": 4,
     "id": "Dante",
-    "view_count": 0,
+    "view_count": 1,
     "like_count": 0,
     "score": 0,
     "rating_total": 0,
