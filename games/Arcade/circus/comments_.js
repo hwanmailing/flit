34095a673b824d97487e082c338ae9de9dd26e18
hwanmailing <hwanmailing@github.com>
2025-11-11 22:28:24 +0900

    testing

--- games/Arcade/circus/comments_.js
+++ games/Arcade/circus/comments_.js
@@ -1,1 +1,12 @@
-window.GAME_COMMENTS = [];
+window.GAME_COMMENTS = [
+  {
+    "idx": 1,
+    "game_id": "RetroCircus",
+    "user_idx": 1,
+    "content": "테스트",
+    "created_at": "2025-11-11 13:26:13",
+    "nickname": "FIREBALL",
+    "picture": "https://lh3.googleusercontent.com/a/ACg8ocIVHWGxnvf1sa0yXjm969SWpgEWlkEudWKTpm3Cw0RTi-eU7pQLGw=s96-c",
+    "lang": "en"
+  }
+];
